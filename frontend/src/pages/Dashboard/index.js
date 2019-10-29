5d83c25029490a18d846d3239e9fdbf81c8fe875
danielgomesp <danielgomesp@gmail.com>
2019-10-29 14:06:59 -0300

    Eslint changes

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -4,33 +4,33 @@ import api from '../../services/api';
 import './styles.css';
 
 export default function Dashboard() {
-    const [spots, setSpots] = useState([]);
+  const [spots, setSpots] = useState([]);
 
-    useEffect(() => {
-        async function loadSpots() {
-            const user_id = localStorage.getItem('user');
-            const response = await api.get('/dashboard', {
-                headers: { user_id }
-            });
-            setSpots(response.data);
-        }
-        loadSpots();
-    }, [])
+  useEffect(() => {
+    async function loadSpots() {
+      const userId = localStorage.getItem('user');
+      const response = await api.get('/dashboard', {
+        headers: { userId },
+      });
+      setSpots(response.data);
+    }
+    loadSpots();
+  }, []);
 
-    return (
-        <>
-            <ul className="spot-list">
-                {spots.map(spot => (
-                    <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.Thumbnail_url})` }} />
-                        <strong>{spot.company}</strong>
-                        <span>{spot.price ? `$${spot.price}/day` : 'Free'}</span>
-                    </li>
-                ))}
-            </ul>
-             <Link to="/new">
-                <button className="btn"> Add new spot </button>
-             </Link>
-        </>
-    )
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map((spot) => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.Thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `$${spot.price}/day` : 'Free'}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/new">
+        <button type="button" className="btn"> Add new spot </button>
+      </Link>
+    </>
+  );
 }
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,31 +2,37 @@ import React, { useState } from 'react';
 import api from '../../services/api';
 
 export default function Login({ history }) {
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const response = await api.post('/sessions', { email })
-        const { _id } = response.data;
-        localStorage.setItem('user', _id);
-        history.push('/dashboard');
-    }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post('/sessions', { email })
+    const { _id } = response.data;
+    localStorage.setItem('user', _id);
+    history.push('/dashboard');
+  }
 
-    return (
-        <>
-            <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.</p>
+  return (
+    <>
+      <p>
+          Ofereça
+        <strong> spots </strong>
+         para programadores e encontre
+        <strong> talentos </strong>
+         para sua empresa.
+      </p>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">E-MAIL *</label>
-                <input
-                    id="email"
-                    type="email"
-                    placeholder="Seu melhor e-mail"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <button type="submit" className="btn">Entrar</button>
-            </form>
-        </>
-    )
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" className="btn">Entrar</button>
+      </form>
+    </>
+  );
 }
